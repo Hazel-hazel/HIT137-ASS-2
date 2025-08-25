c52bbca8650b0537c95c1a769928b83e42f143be
drewski72 <s383691@students.cdu.edu.au>
2025-08-25 23:11:01 +1000

    Add files via upload

---
+++ HIT137_DAN_EXT40_A2Q1.py
@@ -0,0 +1,31 @@
+"""
+Hit137: Team Dan_Ext40
+Assessment 2: Question 1
+"""
+
+def main():
+    """The main function of this program"""
+
+    shift1 = int (input("Please enter first shift number: "))
+    shift2 = int (input("Please enter second shift number: "))
+
+    f = open ("raw_text.txt", "r")
+    
+
+def encrypt():
+    """Encrypts the raw_txt file"""
+
+    return
+
+def decrypt():
+    """Decrypts the encrypted_text file"""
+    return
+
+def verify():
+    """Verifys the decrypted_text file with the raw_text file """
+
+    return
+
+
+main()
+
