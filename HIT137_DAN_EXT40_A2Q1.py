16a609de1658c8121a4c1ce6cb15b87e258d06e6
drewski72 <s383691@students.cdu.edu.au>
2025-08-26 00:09:38 +1000

    Update HIT137_DAN_EXT40_A2Q1.py

--- HIT137_DAN_EXT40_A2Q1.py
+++ HIT137_DAN_EXT40_A2Q1.py
@@ -10,7 +10,16 @@ def main():
     shift2 = int (input("Please enter second shift number: "))
 
     f = open ("raw_text.txt", "r")
+    key1 = shift1 * shift2
+    key2 = - shift1 + - shift2
+    key3 = - shift1
+    key4 = shift2 ** 2
+
+    f = open ("raw_text.txt", "r")
+    raw_text = f.read()
     
+    print (raw_text) # Test mode
+    print (key1, key2, key3, key4) # Test mode
 
 def encrypt():
     """Encrypts the raw_txt file"""
@@ -28,4 +37,5 @@ def verify():
 
 
 main()
+
 
