dd006eade6ca30c32e6fae24c3ac7564de473bb4
drewski72 <s383691@students.cdu.edu.au>
2025-08-26 00:37:04 +1000

    Update HIT137_DAN_EXT40_A2Q1.py

--- HIT137_DAN_EXT40_A2Q1.py
+++ HIT137_DAN_EXT40_A2Q1.py
@@ -4,38 +4,57 @@ Assessment 2: Question 1
 """
 
 def main():
+
     """The main function of this program"""
 
     shift1 = int (input("Please enter first shift number: "))
     shift2 = int (input("Please enter second shift number: "))
-        
+
     key1 = shift1 * shift2
     key2 = - shift1 + - shift2
     key3 = - shift1
     key4 = shift2 ** 2
+    
+    print (key1, key2, key3, key4) # Test mode
+      
+    """Encrypt the raw_text"""
+    
+    encrypt()
+
+    """Decrypt the encrypted_text"""
+    
+    decrypt()
+
+    """Compare decrypted_text"""
+    
+    verify()
+
+def encrypt():
+
+    """Function to encrypt the raw_txt file"""
 
     f = open ("raw_text.txt", "r")
     raw_text = f.read()
-    
+
     print (raw_text) # Test mode
-    print (key1, key2, key3, key4) # Test mode
-
-def encrypt():
-    """Encrypts the raw_txt file"""
-
+    
     return
 
 def decrypt():
-    """Decrypts the encrypted_text file"""
+
+    """Function to decrypt the encrypted_text file"""
+
     return
 
 def verify():
-    """Verifys the decrypted_text file with the raw_text file """
+
+    """Function to verify the decrypted_text file matches the raw_text file """
 
     return
 
-
 main()
+
+
 
 
 
