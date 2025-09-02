e2a782061d55e962f722059f5136894983d9bd4a
drewski72 <s383691@students.cdu.edu.au>
2025-09-02 19:42:29 +1000

    Update Assignment2Q3.py
    added polygon centreing

--- Assignment2Q3.py
+++ Assignment2Q3.py
@@ -1,4 +1,5 @@
 import turtle 
+import math
 
 def draw_edge(t, length, depth):
 
@@ -22,17 +23,26 @@ def draw_polygon(t, sides, length, depth):
         t.right(exterior_turn)
 
 def main():
-    sides = int(input("Enter number of sides:"))
-    length = int(input("Enter side length (pixels):"))
-    depth = int(input("Enter recursion depth:"))
+    sides = int(input("Enter number of sides: "))
+    length = int(input("Enter side length (pixels): "))
+    depth = int(input("Enter recursion depth: "))
+    R = length / (2* math.sin(math.pi / sides))  # Calculate radius of polygon
 
     screen = turtle.Screen()
     t= turtle.Turtle ()
     t.speed(0)
+
+    t.pensize(2)  # Set width of pen tip
+    t.up ()  # Pen up, so it doesn't draw while turtle is moved to start position
+    t.left (90+ (360 / (2 * sides))) # Set polygon vertex intercept angle
+    t.forward(R) # Send turtle one polygon radius to start position 
+    t.setheading(0) # Face turtle East
+    t.down () # Pen down, so it can draw 
 
     draw_polygon(t, sides, length, depth)
 
     screen.mainloop()
 
 if __name__ == "__main__":
+
     main()
