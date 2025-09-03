f5eb75ce47d8f0831929df8e6da9acbcb86d0bcb
drewski72 <s383691@students.cdu.edu.au>
2025-09-04 01:34:41 +1000

    Update HIT137_DAN_EXT40_A2Q1.py

--- HIT137_DAN_EXT40_A2Q1.py
+++ HIT137_DAN_EXT40_A2Q1.py
@@ -1,166 +1,185 @@
 """
 Hit137: Team Dan_Ext40
 
-Assessment 2: Question 1
+Assignment 2: Question 1
 
 Program: Ass2_Q1.py
 
 Authors: Maharun Momo Islam, Moneya Islam, Andrew Morris, Kudzaishe Mutyasira
-Last date modified: 30/08/25
+
+Last date modified: 04/09/25
 
 Encryption / Decryption:
-The purpose of this program is to accept two shift numbers from the user and 
-use these to create four keys that will shift the characters up or down 
-depending on their case and position in the alphabet. 
-encrypt a text file and save it, load that file decrypt it, and save it. It 
-will then load the original text file and verify that it matches the 
+
+The purpose of this program is to encrypt a text file by accepting two shift 
+numbers from the user and use them to create four keys that will shift the 
+characters up or down depending on their case and position in the alphabet.
+In the process it will create a fifth key to allow decryption. As there is 
+no wrap around of characters, this allows shifting well beyond the alphabet, 
+making simple shift reversal during decryption difficult without the fifth 
+key, adding an extra layer of security. It will then save the encrypted text
+file, load that file, decrypt it with the five keys, and save it again. It
+will then load the original text file and verify that it matches the
 decrypted file.
 
 """
 
+# Accept shift numbers from user
 shift1 = int (input("Please enter first shift number: "))
 shift2 = int (input("Please enter second shift number: "))
 
+# Create encryption keys    
 key1 = shift1 * shift2
 key2 = - shift1 + - shift2
 key3 = - shift1
 key4 = shift2 ** 2
 
+
 def main():
+
     """The main function of this program"""
-        
-    """Show keys"""
-    print (key1, key2, key3, key4)  # Test mode 
    
-    """Encrypt the raw_text"""
-    encrypt()    
-        
-    print ("File encrypted!")
-    print ("Encrypted text file saved.")
     
-    """Decrypt the encrypted_text"""
-    decrypt()
+    encrypt()  # Encrypt raw_text.txt file
+            
+    print("File encrypted!")
+    print("Encrypted text file saved.")
+        
+    
+    decrypt()  # Decrypt encrypted_text.txt file
 
-    print ("File decrypted!")
-    print ("Decrypted text file saved.")
+    print("File decrypted!")
+    print("Decrypted text file saved.")
 
-    """Compare decrypted_text"""
-    verify()
-
-
-    """Define functions"""
+    
+    verify()  # Compare decrypted_text to original raw_text file
+   
 
 def encrypt():
-    """Function to encrypt the raw_txt file and save as encrypted_text.txt"""
 
-    file_raw = open ("raw_text.txt", "r")
+    """Function to encrypt the raw_txt.txt file and save as encrypted_text.txt"""
+
+    file_raw = open("raw_text.txt", "r")
     raw_text = file_raw.read()
 
     encrypted_text = ""
-    key_text = ""
-    for ch in raw_text:
-        if ch >= chr(97) and ch <= chr(109):
-            echr = ord(ch) + key1
-            encrypted_text = encrypted_text + chr(echr)
-            key_text = key_text + "1"
-        elif ch >= chr(110) and ch <= chr(122):
-            echr = ord(ch) + key2
-            encrypted_text = encrypted_text + chr(echr)
-            key_text = key_text + "2"
-        elif ch >= chr(65) and ch <= chr(77):
-            echr = ord(ch) + key3
-            encrypted_text = encrypted_text + chr(echr)
-            key_text = key_text + "3"
-        elif ch >= chr(78) and ch <= chr(90):
-            echr = ord(ch) + key4
-            encrypted_text = encrypted_text + chr(echr)
-            key_text = key_text + "4"
-        else:
-            encrypted_text = encrypted_text + ch
-            key_text = key_text + "0"
+    key5 = ""
 
-    """ Test to compare input and output"""
-    print (raw_text)  # Test mode
-    print (encrypted_text)  # Test mode
-    print (key_text)  # Test mode
-
-    """Save encrypted file"""
     
-    file_e = open ("encrypted_text.txt", "w", encoding = "utf-8")
-    file_e.write (str(encrypted_text))
+    for ch in raw_text:  # setup a loop to process one character at a time from loaded raw_text file
+       
+        if ch >= chr(97) and ch <= chr(109):  # First half of lower case alphabet rule, (a - m)
+            echr = ord(ch) + key1  # Shift character with key1
+            encrypted_text = encrypted_text + chr(echr) # Add shifted character to variable
+            key5 = key5 + "1" # Add key number to key sequence
+
+        elif ch >= chr(110) and ch <= chr(122):  # Second half of lower case alphabet rule, (n - z)
+            echr = ord(ch) + key2   # Shift character with key2
+            encrypted_text = encrypted_text + chr(echr)  # Add shifted character to variable
+            key5 = key5 + "2"   # Add key number to key sequence
+
+        elif ch >= chr(65) and ch <= chr(77):  # First half of upper case alphabet rule, (A - M)
+            echr = ord(ch) + key3   # Shift character with key3
+            encrypted_text = encrypted_text + chr(echr)   # Add shifted character to variable
+            key5 = key5 + "3"  # Add key number to key sequence
+
+        elif ch >= chr(78) and ch <= chr(90):  # Second half of upper case alphabet rule, (N -Z)
+            echr = ord(ch) + key4   # Shift character with key4
+            encrypted_text = encrypted_text + chr(echr)   # Add shifted character to variable
+            key5 = key5 + "4"  # Add key number to key sequence
+        else:
+            encrypted_text = encrypted_text + ch  # Spaces, tabs, newlines, special characters, and numbers remain unchanged
+            key5 = key5 + "0"  # Add key number to key sequence (0 = no key)
+
+    # Test mode
+    print (key1, key2, key3, key4)
+    print(raw_text)  
+    print(encrypted_text) 
+    print(key5)  
+
+    # Save encrypted_text file    
+    file_e = open("encrypted_text.txt", "w", encoding = "utf-8")  # encoding allows extended character range
+    file_e.write(str(encrypted_text))
     file_e.close
-    file_e = open ("key_text.txt", "w")
-    file_e.write (str(key_text))
-    file_e.close     
-        
+
+    # Save key_text file
+    file_k = open("key5.txt", "w")
+    file_k.write(str(key5))
+    file_k.close     
+
+
 def decrypt():
+
     """Function to decrypt the encrypted_text file"""
 
-    file_e = open ("encrypted_text.txt", "r", encoding = "utf-8")
+    file_e = open("encrypted_text.txt", "r", encoding = "utf-8")  # encoding allows extended character range
     etext = file_e.read()
-    file_e = open ("key_text.txt", "r")
-    ktext = file_e.read()
-    print ("Encrypted text file loaded")
-    
-    print (etext)  # Test mode
-    
+
+    file_k = open("key5.txt", "r")
+    ktext = file_k.read()
+
+    print("Encrypted text file loaded")
+
     decrypted_text = ""
-    y = 0
+    key_sequence = 0
 
-    for echr in etext:
-        x = ktext[y]
-        if x == "1":
-            dchr = ord(echr) - key1
-            decrypted_text = decrypted_text +chr(dchr)
-            y = y + 1
-        elif x == "2":
-            dchr = ord(echr) - key2
-            decrypted_text = decrypted_text +chr(dchr)  
-            y = y + 1
-        elif x == "3":
-            dchr = ord(echr) - key3
-            decrypted_text = decrypted_text +chr(dchr) 
-            y = y + 1
-        elif x == "4":
-            dchr = ord(echr) - key4 
-            decrypted_text = decrypted_text +chr(dchr) 
-            y = y + 1
-        elif x == "0":
-            dchr = ord(echr) - 0
-            decrypted_text = decrypted_text +chr(dchr) 
-            y = y + 1    
     
-    print (decrypted_text)   
+    for echr in etext:  # setup a loop to process one character at a time from loaded encrypted_text file
 
-    """ Test to compare input and output"""
+        key5 = ktext[key_sequence]  # Step through key5 elements
 
-    print (decrypted_text)  # Test mode
+        if key5 == "1":  # Select appropriate key for decryption 
+            dchr = ord(echr) - key1  # Reverse decryption with key1
+            decrypted_text = decrypted_text +chr(dchr)  # Add decrypted character to variable
+            key_sequence = key_sequence + 1  # Progress forward through key5 sequence
 
-    """Save decrypted file"""
+        elif key5 == "2":  # Select appropriate key for decryption 
+            dchr = ord(echr) - key2  # Reverse decryption with key2
+            decrypted_text = decrypted_text +chr(dchr)  # Add decrypted character to variable 
+            key_sequence = key_sequence + 1  # Progress forward through key5 sequence
+
+        elif key5 == "3":  # Select appropriate key for decryption 
+            dchr = ord(echr) - key3  # Reverse decryption with key3
+            decrypted_text = decrypted_text +chr(dchr)  # Add decrypted character to variable
+            key_sequence = key_sequence + 1  # Progress forward through key5 sequence
+
+        elif key5 == "4":  # Select appropriate key for decryption 
+            dchr = ord(echr) - key4  # Reverse decryption with key4
+            decrypted_text = decrypted_text +chr(dchr)  # Add decrypted character to variable
+            key_sequence = key_sequence + 1  # Progress forward through key5 sequence
+
+        elif key5 == "0":  # Select appropriate key for decryption 
+            dchr = ord(echr) - 0  # No decryption required
+            decrypted_text = decrypted_text +chr(dchr)  # Add unmodified character to variable
+            key_sequence = key_sequence + 1  # Progress forward through key5 sequence  
     
-    file_d = open ("decrypted_text.txt", "w")
-    file_d.write (str(decrypted_text))
+    # Test mode
+    print(decrypted_text)  
+    
+    # Save decrypted text file    
+    file_d = open("decrypted_text.txt", "w")
+    file_d.write(str(decrypted_text))
     file_d.close
-
     
 
 def verify():
+
     """Function to verify the decrypted_text file matches the raw_text file """
 
-    file_r = open ("raw_text.txt", "r")
+    file_r = open("raw_text.txt", "r")
     raw_text = file_r.read()
 
-    file_d = open ("decrypted_text.txt", "r")
+    file_d = open("decrypted_text.txt", "r")
     d_file = file_d.read()
 
     if raw_text == d_file:
-        print ("File verified!")
+        print("File verified!")
+
     else:
-        print ("Verification error!")
+        print("Verification error!")
+
     
 main()
-
-
 
 
 
