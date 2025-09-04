3278e9b41b0b366399374f777f02dee4ca9a6917
drewski72 <s383691@students.cdu.edu.au>
2025-09-05 00:50:19 +1000

    Update Assignment2Q3.py
    Spaced it out a bit, fixed some notes, moved main function to top of code

--- Assignment2Q3.py
+++ Assignment2Q3.py
@@ -1,5 +1,31 @@
 import turtle 
-import math
+import math  # Needed to centre polygon
+
+def main():
+    sides = int(input("Enter number of sides: "))
+    length = int(input("Enter side length (pixels): "))
+    depth = int(input("Enter recursion depth: "))
+    colour = (input("Enter a colour: "))
+    width = int(input("Enter a pen tip width: "))
+    
+    R = length / (2* math.sin(math.pi / sides))  # Calculate radius of polygon
+
+    screen = turtle.Screen()
+    t= turtle.Turtle ()
+    t.speed(0)
+
+    t.up ()  # Pen up, so it doesn't draw while turtle is moved to start position
+    t.left (90+ (360 / (2 * sides))) # Set polygon vertex intercept angle
+    t.forward(R) # Send turtle one polygon radius to start position 
+    t.setheading(0) # Face turtle East
+    t.down () # Pen down, so it can draw
+    t.pensize(width)  # Set width of pen tip
+    t.pencolor(colour)  # Set colour of pen 
+
+    draw_polygon(t, sides, length, depth)
+
+    screen.mainloop()
+
 
 def draw_edge(t, length, depth):
 
@@ -16,40 +42,21 @@ def draw_edge(t, length, depth):
     t.right(60)
     draw_edge(t, segment, depth - 1)
 
+
 def draw_polygon(t, sides, length, depth):
     exterior_turn = 360.0 / sides 
     for _ in range(sides):
         draw_edge(t, length, depth)
         t.right(exterior_turn)
 
-def main():
-    sides = int(input("Enter number of sides: "))
-    length = int(input("Enter side length (pixels): "))
-    depth = int(input("Enter recursion depth: "))
-    colour = (input("Enter a colour: "))
-    width = int(input("Enter a pen tip width: "))
-    
-    R = length / (2* math.sin(math.pi / sides))  # Calculate radius of polygon
-
-    screen = turtle.Screen()
-    t= turtle.Turtle ()
-    t.speed(0)
-
-    #t.pensize(2)  # Set width of pen tip
-    t.up ()  # Pen up, so it doesn't draw while turtle is moved to start position
-    t.left (90+ (360 / (2 * sides))) # Set polygon vertex intercept angle
-    t.forward(R) # Send turtle one polygon radius to start position 
-    t.setheading(0) # Face turtle East
-    t.down () # Pen down, so it can draw
-    t.pensize(width)  # Set width of pen tip
-    t.pencolor(colour)  # Set colour of pen 
-
-    draw_polygon(t, sides, length, depth)
-
-    screen.mainloop()
 
 if __name__ == "__main__":
-
     main()
+
+
+
+
+
+
 
 
